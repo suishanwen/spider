2fec23b82dcd35f59f004a40a5ef1eba87bb09b4
suishanwen <suishanwen@icloud.com>
2019-02-26 12:03:11 +0800

    add opt

---
+++ ReadMe.md
@@ -0,0 +1,6 @@
+pip3 install pyyaml
+pip3 install pymysql
+pip3 install selenium
+pip3 install requests
+
+yum install chromeDriver
--- util/chrome.py
+++ util/chrome.py
@@ -4,6 +4,8 @@ from conf.config import Const
 
 def get_chrome():
     option = webdriver.ChromeOptions()
+    # option.add_argument('--headless')
+    option.add_argument('--no-sandbox')
     prefs = {"download.default_directory": Const.DOWNLOAD_PATH, "download.prompt_for_download": False, }
     option.add_experimental_option("prefs", prefs)
     # option.add_argument("--headless")
