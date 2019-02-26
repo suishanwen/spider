9ef476078505ece45a0433d056ad22aca15e4728
suishanwen <suishanwen@icloud.com>
2019-02-26 12:49:14 +0800

    enable_download_in_headless_chrome

--- util/chrome.py
+++ util/chrome.py
@@ -6,9 +6,17 @@ def get_chrome():
     option = webdriver.ChromeOptions()
     option.add_argument('--headless')
     option.add_argument('--no-sandbox')
-    prefs = {"download.default_directory": Const.DOWNLOAD_PATH, "download.prompt_for_download": False, }
-    option.add_experimental_option("prefs", prefs)
-    # option.add_argument("--headless")
-    return webdriver.Chrome(
+    option.add_experimental_option("prefs", {"download.default_directory": Const.DOWNLOAD_PATH,
+                                             "download.prompt_for_download": False, })
+    driver = webdriver.Chrome(
         chrome_options=option)
-    # return webdriver.Chrome()
+    enable_download_in_headless_chrome(driver, Const.DOWNLOAD_PATH)
+    return driver
+
+
+def enable_download_in_headless_chrome(driver, download_dir):
+    # add missing support for chrome "send_command"  to selenium webdriver
+    driver.command_executor._commands["send_command"] = ("POST", '/session/$sessionId/chromium/send_command')
+
+    params = {'cmd': 'Page.setDownloadBehavior', 'params': {'behavior': 'allow', 'downloadPath': download_dir}}
+    command_result = driver.execute("send_command", params)
