01c7db67854dbe814e333a20c8adf1b3de155453
suishanwen <suishanwen@icloud.com>
2019-03-05 10:56:54 +0800

    修改附件下载

--- page/NhcSt.py
+++ page/NhcSt.py
@@ -61,5 +61,4 @@ class NhcSt(PageInfo):
         for _class in con_classes:
             for ext in _chrome.chrome.find_elements_by_class_name(_class):
                 ext_a_list += ext.find_elements_by_tag_name("a")
-                ext_a_list += ext.find_elements_by_tag_name("img")
         return ext_a_list
