14ea312d39edba99dc0d910e8311da9ee6893bfb
suishanwen <suishanwen@icloud.com>
2019-03-07 15:47:12 +0800

    附件下载重构

--- model/Attachment.py
+++ model/Attachment.py
@@ -1,7 +1,7 @@
 class Attachment(object):
     def __init__(self, url, file_name, file_type_name, file_path, local_href):
-        self.url = ""
-        self.file_name = ""
-        self.file_type_name = ""
-        self.file_path = ""
-        self.local_href = ""
+        self.url = url
+        self.file_name = file_name
+        self.file_type_name = file_type_name
+        self.file_path = file_path
+        self.local_href = local_href
