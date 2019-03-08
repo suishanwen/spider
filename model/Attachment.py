d128ec0c41c32eb5d4ee18cfac6fd171591acdd9
suishanwen <suishanwen@icloud.com>
2019-03-08 10:54:14 +0800

    添加附件关联

--- model/Attachment.py
+++ model/Attachment.py
@@ -1,9 +1,8 @@
 class Attachment(object):
-    def __init__(self, url, file_name, relate_file_name, file_type_name, file_path, local_href):
+    def __init__(self, url, file_name, file_type_name, file_path, local_href):
         self.url = url
         self.file_name = file_name
         # 同源文件
-        self.relate_file_name = relate_file_name
         self.file_type_name = file_type_name
         self.file_path = file_path
         self.local_href = local_href
