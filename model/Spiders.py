d102cec3c869aa3d69ec512094c576487766be61
suishanwen <suishanwen@icloud.com>
2019-03-12 16:22:22 +0800

    add Beijing

--- model/Spiders.py
+++ model/Spiders.py
@@ -1,7 +1,9 @@
 from spiders.NhcPublic import NhcPublic
 from spiders.NhcSt import NhcSt
+from spiders.Beijing import Beijing
 
 Spiders = {
     "NhcSt": NhcSt(),
     "NhcPublic": NhcPublic(),
+    "Beijing": Beijing(),
 }
